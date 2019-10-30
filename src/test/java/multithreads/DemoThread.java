1325f124f6267d7d58ea7f495185070cb6ce207a
Iurii <kenu21@gmail.com>
2019-10-30 13:11:21 +0200

    hw-36-1: without variable.

--- src/test/java/multithreads/DemoThread.java
+++ src/test/java/multithreads/DemoThread.java
@@ -3,9 +3,7 @@ package multithreads;
 public class DemoThread {
     private static Counter counter = new Counter();
     public static void main(String[] args) throws InterruptedException {
-        MyThreadR myThreadR = new MyThreadR(counter);
-        MyThreadE myThreadE = new MyThreadE(counter);
-        myThreadE.start();
-        myThreadR.getThread().start();
+        new MyThreadE(counter).start();
+        new MyThreadR(counter).getThread().start();
     }
 }
